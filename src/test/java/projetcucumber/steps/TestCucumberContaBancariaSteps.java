a4ec8e18ded434f1f379c7d35b6edcb4593d2186
Viviane Emilie <vivi29emi@gmail.com>
2022-08-30 18:24:46 -0300

    Adicionado teste conta poupança

--- src/test/java/projetcucumber/steps/TestCucumberContaBancariaSteps.java
+++ src/test/java/projetcucumber/steps/TestCucumberContaBancariaSteps.java
@@ -5,15 +5,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import projetcucumber.contabancaria.ContaBancaria;
+import projetcucumber.contabancaria.ContaPoupanca;
 
 public class TestCucumberContaBancariaSteps {
 	
 	private ContaBancaria conta;
+	private ContaPoupanca contaPoupanca;
 
 	
-	@Dado("cliente {string} tem de saldo R$ {double}")
+	@Dado("{string} tem o saldo R$ {double}")
 	public void cliente_tem_de_saldo_r$(String cliente, Double saldoConta) {
         
 		conta = new ContaBancaria ();
@@ -22,59 +25,85 @@ public class TestCucumberContaBancariaSteps {
 	  
 	}
 	
-	@Quando("deposita um valor R$ {double}")
-	public void deposita_um_valor_r$(Double valor) {
+	@Quando("deposita R$ {double}")
+	public void deposita_r$(Double valor) {
 		 
 		 
 		 if (conta.depositar(valor)) {
+			 
 				conta.getSaldo();
 				
-				System.out.println("Valor depositado com sucesso! saldo atual " + conta.getSaldo());
-			
-				
 			}else {
-				System.out.println(" O valor não foi depositado" + conta.getSaldo());
+				 conta.equals(null);
 			}
 	  
 	}
 
 
-	@Entao("o valor é depositado e multiplicado tendo o total de R$ {double} na sua conta com sucesso")
-	public void o_valor_é_depositado_e_multiplicado_tendo_o_total_de_r$_na_sua_conta_com_sucesso(Double saldoBancaria) {
+	@Entao("o saldo e alterado R$ {double}")
+	public void o_saldo_e_alterado(Double saldoBancaria) {
 	 
 	assertThat(conta.getSaldo(),is(saldoBancaria));
 	}
 
-	@Dado("que cliente {string} tem de saldo R$ {double}")
-	public void que_cliente_tem_de_saldo_r$(String cliente, Double saldoConta) {
+	@Dado("que {string} possui o saldo R$ {double}")
+	public void que_possui_o_saldo_r$(String cliente, Double saldoConta) {
 		conta = new ContaBancaria ();
 		conta.setNomeCliente(cliente);
 		conta.setSaldo(saldoConta);
 	   
 	}
 
-	@Quando("efetua um saque no valor R$ {double}")
-	public void efetua_um_saque_no_valor_r$(Double valor) {
+	@Quando("efetua um saque R$ {double}")
+	public void efetua_um_saque_r$(Double valor) {
 	 
 		 if (conta.sacar(valor)) {
+			 
 			 conta.getSaldo();
 				
-				System.out.println("saque efetuado com sucesso! saldo atual " + conta.getSaldo());
-			
-				
 			}else {
-				System.out.println(" saldo indisponível " + valor);
+				 conta.equals(null);
 			}
 	}
 
-	@Entao("o valor é  subtraido tendo um total de R$ {double} na sua conta com sucesso")
-	public void o_valor_é_subtraido_tendo_um_total_de_r$_na_sua_conta_com_sucesso(Double saldoBancario) {
+	@Entao("o saldo da conta reduz R$ {double}")
+	public void o_saldo_da_conta_reduz(Double saldoBancario) {
 	   
 		
 		assertThat(conta.getSaldo(),is(saldoBancario));
 		
 	}
 	
+	// Teste de deposito em conta poupança e tem rendimento 
+	
+	@Dado("que {string}")
+	public void que(String cliente) {
+		contaPoupanca = new ContaPoupanca();
+		contaPoupanca.setNomeCliente(cliente);
+	    
+	}
+	@Quando("deposita R$ {int} no dia {int} é aplicado o rendimento {double} porcento")
+	public void deposita_R$_no_dia_e_aplicado_o_rendimento_porcento(Integer valor, Integer diaRendiment, Double txRendiment) {
+		contaPoupanca.depositar(valor);
+		contaPoupanca.setDiaRendimento(diaRendiment);
+		
+		if(contaPoupanca.calcularNovoSaldo(txRendiment)) {
+		     contaPoupanca.getSaldo();
+			
+		}else {
+			
+			contaPoupanca.equals(null);
+		}
+	
+		
+	}
+	@Então("o saldo  vai para R$ {double}")
+	public void o_saldo_vai_para_R$(Double saldoPoupanca) {
+	    
+		assertThat(contaPoupanca.getSaldo(),is(saldoPoupanca));
+		
+	}
+
 
 
 
